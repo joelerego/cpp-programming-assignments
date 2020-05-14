448c5f62a00fba0b2a0aa1a0ac9080455c331423
Joel E. Rego <joelerego@gmail.com>
2020-05-14 23:24:26 +0530

    Example of vectors and templates in CPP

---
+++ summation-template.cpp
@@ -0,0 +1,35 @@
+// The following CPP program contains 
+// a template function that can take 
+// all summable datatypes as input and
+// return the sum.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// #define constants become consts
+const int N = 10;
+
+// The following is a template for universal summation
+template <class T>
+void summation(T& sum, T size, vector<T> data) {
+	for(int i=0; i<size; i++) {
+		sum += data[i];
+	}
+}
+
+int main() {
+	int sum = 0;
+	vector<int> data;
+	
+	for(int i = 0; i < N; ++i) 
+		data.push_back(i);
+	
+	summation(sum, N, data);
+	
+	printf("sum is %d\n", sum);
+	return 0;
+
+}
