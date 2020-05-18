5152d4b8d987b2005712967a1fe13918a11d9819
Joel E. Rego <joelerego@gmail.com>
2020-05-18 09:38:51 +0530

    Added constructors

--- cpp-point.cpp
+++ cpp-point.cpp
@@ -4,12 +4,23 @@ using namespace std;
 
 class point {
 public:
+	point() { 
+		this -> x = 0.0; 
+		this -> y = 0.0; 
+	}
+	point(double x, double y) { 
+		this -> x = x; 
+		this -> y = y; 
+	}
 	double getx() { return x; } // Inline automatically
-	void setx(double v) { x = v } // Mutator method
+	double gety() { return y; } // Inline automatically
+	void setx(double v) { x = v; } // Mutator method 
+	void sety(double v) { y = v; } // Mutator method
 private:
 	double x, y;
 };
 
 int main() {
+	point p1;
 	return 0;
 }
