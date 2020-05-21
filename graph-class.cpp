fb74c996000398617ef5b1a3963747a6c04e32f6
Joel E. Rego <joelerego@gmail.com>
2020-05-21 10:53:17 +0530

    Graph Class
    *Added constructor that uses dynamic allocation for 2D matrix
    
    *Added destructor to retrieve space from the heap

--- graph-class.cpp
+++ graph-class.cpp
@@ -1,8 +1,32 @@
+// This program creates a randomly generated graph.
+// It uses the heap to dynamically allocate memory to
+// a 2D boolean array.
+//
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+// The following is the graph class which creates
+// a dynamically allocated 2D boolean array which
+// represents the existence of an edge from i to j.
+// The matrix is a square matrix, i.e, i equals j and
+// the total number of nodes in the graph is i or j.
+//
+class new_graph {
+public:
+	new_graph(int size = 5) {
+		graph = new bool*[size];
+		for(int i = 0; i < size; ++i)
+			graph[i] = new bool[size];		
+	}
+	~new_graph() {
+		delete graph;
+	}
+private:
+	bool** graph;	
+};
+
 int main() {
-	int a = 4;
-	cout << a << endl;
-	return 0;
+	
 }
